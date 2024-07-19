083c9809e017a4d01e075822be0956fa531b11a0
TayyabAArif <apple@APPLEs-MacBook-Pro.local>
2024-07-19 12:00:40 -0700

    Fix font bold issue

--- components/vendor/knowledge/knowledge-filter/index.jsx
+++ components/vendor/knowledge/knowledge-filter/index.jsx
@@ -194,11 +194,7 @@ const KnowledgeFilter = ({ triggerFunction, setShowFilter, setFilters, filters, 
     <div className="w-full h-full bg-white flex flex-col justify-between">
       <div className="text-[18px]">
         <div className="flex justify-between p-5 items-center border-b-2">
-<<<<<<< Updated upstream
-          <h1 className="2xl:text-[22px] text-[16px]">Filters</h1>
-=======
           <h1 className="2xl:text-[20px] text-[16px] font-bold ">Filter</h1>
->>>>>>> Stashed changes
           <X
             className="size-[16px] cursor-pointer"
             color="#2457d7"
@@ -238,12 +234,12 @@ const KnowledgeFilter = ({ triggerFunction, setShowFilter, setFilters, filters, 
                   setIsSelected(true);
                 }}
               >
-                <h1 className="2xl:text-[20px] text-[16px] font-bold">
+                <h1 className="2xl:text-[20px] text-[16px]">
                   {data.name}
                 </h1>
                 <div className="flex gap-3 ">
                   {filters.find((filter) => filter.name === data.value)?.value.length > 0 ? (
-                    <Circle size={15} strokeWidth={4} color="#2457d7" className="w-[25px]" />
+                    <Circle size={15} strokeWidth={4} color="#2457d7" className="w-[35px]" />
                   ) : (
                     ""
                   )}
