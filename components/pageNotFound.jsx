5976c9a8600e2cded098742c279c9cab1ed78994
tayyabArif <tayyablefty786@gmail.com>
2025-02-20 12:14:56 +0500

    resolve build issue

--- components/pageNotFound.jsx
+++ components/pageNotFound.jsx
@@ -4,7 +4,7 @@ const PageNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
-      <p className="text-xl mb-6">Oops! The page you're looking for doesn't exist.</p>
+      <p className="text-xl mb-6">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
     </div>
   );
 };
