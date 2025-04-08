111de59f323fd0f2c20f00f5ad875578a4853bb4
TayyabAArif <tayyablefty786@gmail.com>
2025-04-08 08:34:03 -0700

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        tilda: ['"Tilda Sans"', 'sans-serif'],
+        tilda: ['"TildaSans"', 'sans-serif'],
       },
       colors: {
         primary: "#2457d7",
